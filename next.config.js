7db03cea053dbef46d216c297d8ab69ec5fb8afb
Edward Kihiro <kihiroedward@gmail.com>
2024-02-16 21:08:18 +0300

    Environment variables

--- next.config.js
+++ next.config.js
@@ -4,6 +4,10 @@ const nextConfig = {
     distDir : 'dist',
     images:{
         unoptimized:true,
+    },
+    env : {
+        RESEND_API_KEY:'re_5eziuYfo_4X8bLYiFmeZnEpXQsZaq41aD',
+        FROM_EMAIL:'eddiefxme@gmail.com'
     }
 }
 
